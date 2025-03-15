962d791c2bc726338f2268634b17bdee3d5a0729
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-03-15 20:18:10 +0530

    added the proper keys and client ID

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,6 @@
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
-  cookieKey: '123123123',
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: "123123123",
 };
